5aa8983aab745c0846e1f0dc7a058715cfbfa965
DasVergissmeinnicht <36331672+DasVergissmeinnicht@users.noreply.github.com>
2018-09-17 18:17:57 +0800

    COMP3111H Fall 2018 Lab2

---
+++ src/main/java/lab2/ex1/Book.java
@@ -0,0 +1,29 @@
+package lab2.ex1;
+
+public class Book {
+	private String chapters[];
+	private static final int DEFAULT_CHAPTERS = 10;
+	public Book() {
+		chapters = new String[DEFAULT_CHAPTERS];
+		for (int i=0; i<chapters.length; i++)
+			chapters[i] = "Chapter " + i;
+	}
+	public Book(String argument[]) {
+		/* construct the object with an array of chapter names */
+		int length = argument.length;
+		chapters = new String[length];
+		for (int i=0; i<length; i++)
+			chapters[i] = argument[i];
+	}
+	public String getChapter(int i) {
+		/*return the chapter by the given index. if the index is <0 or >= to the array size
+		 * return "invalid chapter"
+		 */
+		if (i < 0 || i>=chapters.length)
+			return "invalid chapter";
+		return chapters[i];
+	}
+	public String[] getChapters() {
+		return chapters;
+	}
+}
