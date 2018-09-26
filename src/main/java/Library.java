ce4e3dc40a42f0e00457ee9926ea63de959e356a
DasVergissmeinnicht <36331672+DasVergissmeinnicht@users.noreply.github.com>
2018-09-26 14:26:46 +0800

    Add Lab3 Part Here

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -5,31 +5,34 @@ import lab2.ex2.*;
  */
 public class Library {
 	/* Add this function */
+//	public static void main(String arg[]) {
+//		// System.out.println("When there is a fire, commit and push.");
+//		final String array[] = {"Basic Java", "Advance Java", "Forget about Java"};
+//		Book b = new Book(array);
+//		System.out.println("The title of Chapter 1 is " + b.getChapter(1));
+//		String anotherArray[] = b.getChapters();
+//		
+//		System.out.println("There are " + anotherArray.length + " chapters.");
+//		/* Basic */
+//		// System.out.println(anotherArray);
+//		/* Another */
+//		System.out.println(java.util.Arrays.toString(anotherArray));
+//		
+//		/** Add the following **/
+//		Computer a = new MobileComputer();
+//		for (int i = 0; i<10; i++)
+//			a.work();
+//		
+//		Charger c = new Charger();
+//		Phone p = new Phone();
+//		MobileComputer m = new MobileComputer();
+//		c.charge(p);
+//		c.charge(m);
+//	}
+//    public boolean someLibraryMethod() {
+//        return true;
+//    }
 	public static void main(String arg[]) {
-		// System.out.println("When there is a fire, commit and push.");
-		final String array[] = {"Basic Java", "Advance Java", "Forget about Java"};
-		Book b = new Book(array);
-		System.out.println("The title of Chapter 1 is " + b.getChapter(1));
-		String anotherArray[] = b.getChapters();
-		
-		System.out.println("There are " + anotherArray.length + " chapters.");
-		/* Basic */
-		// System.out.println(anotherArray);
-		/* Another */
-		System.out.println(java.util.Arrays.toString(anotherArray));
-		
-		/** Add the following **/
-		Computer a = new MobileComputer();
-		for (int i = 0; i<10; i++)
-			a.work();
-		
-		Charger c = new Charger();
-		Phone p = new Phone();
-		MobileComputer m = new MobileComputer();
-		c.charge(p);
-		c.charge(m);
+		lab3.ex2.UIApplication.run(arg);
 	}
-    public boolean someLibraryMethod() {
-        return true;
-    }
 }
